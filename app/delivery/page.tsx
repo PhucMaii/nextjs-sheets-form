a9e64fb518d0fe8558ced5d32dd7b877445f16a1
PhucMaii <maithienphuc0102@gmail.com>
2023-11-30 01:58:23 -0700

    Change data structure

---
+++ app/delivery/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React, { useState } from 'react'
+
+export default function CashFlow() {
+  const [expense, setExpense] = useState<number>(0);
+  const [revenue, setRevenue] = useState<number>(0);
+  const handleSubmit = async () => {
+    const form = [
+      {
+        sheetName: 'MAIN',
+        row: 3,
+        revenue
+      },
+      {
+        sheetName: 'DELIVERY_APP',
+        row: 1,
+        expense,
+        revenue
+      }
+    ];
+    try {
+        const response: any = await fetch('/api/expense', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(form)
+        })
+        const content: any = await response.json();
+        alert(content.data);
+        console.log(content);
+    } catch(error) {
+        console.log(error);
+    }
+  }
+  return (
+    <div className="max-w-5xl mx-auto py-16">
+      <h4 className="px-8">Delivery Input Form</h4>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="expense"
+            >
+              Expense
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="expense"
+              type="number"
+              placeholder="Name"
+              value={expense}
+              onChange={(e) => setExpense(+e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="revenue"
+            >
+              Revenue
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="revenue"
+              type="number"
+              placeholder="Revenue"
+              value={revenue}
+              onChange={(e) => setRevenue(+e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+    </div>
+  )
+}
--- pages/api/expense/index.ts
+++ pages/api/expense/index.ts
@@ -1,14 +1,72 @@
+import { google } from "googleapis";
 import { NextApiRequest, NextApiResponse } from "next";
 
 interface SheetForm  {
+    sheetName: string
+    row: number
     expense: number
     revenue: number
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method !== "POST") {
         return res.status(500).send("Only Post method allowed");
     }
-    const body: SheetForm = req.body;
-    
+    const body: Array<SheetForm> = req.body;
+    /*
+    * should receive in a form [
+        {
+            sheetName: MAIN,
+            row: 5,
+            expense
+        }
+    ]
+    */
+    console.log(body);
+    try {
+        const auth = new google.auth.GoogleAuth({
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+            },
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/spreadsheets'
+            ]
+        })
+        const sheets = google.sheets({
+            auth,
+            version: 'v4'
+        })
+
+        const data = [];
+        const columns = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+        const expenseRow = 5;
+        for (const sheet of body) {
+            const response = await sheets.spreadsheets.values.get({
+              spreadsheetId: process.env.GOOGLE_SHEET_ID,
+              range: sheet.sheetName, // Assuming you want to retrieve all data from columns B to E
+            });
+            const numCols = response.data.values ? response.data.values[sheet.row - 1].length : 0;
+            const appendResponse = await sheets.spreadsheets.values.append({
+                spreadsheetId: process.env.GOOGLE_SHEET_ID,
+                range: `${sheet.sheetName}!${columns[numCols]}${sheet.row}`,
+                valueInputOption: "USER_ENTERED",
+                requestBody: {
+                    values: [
+                        [sheet.expense?.toString()],
+                        [sheet.revenue?.toString()]
+                    ]
+                }                
+            })
+            data.push(appendResponse.data);
+        }
+        return res.status(200).json({
+            data
+        })
+        
+    } catch(error) {
+        console.log(error);
+    }
 }
