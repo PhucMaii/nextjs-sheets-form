fa3414fdfca1ffdab78297f6fd6e1fed52454834
PhucMaii <maithienphuc0102@gmail.com>
2024-02-25 11:11:44 -0800

    Remove unnecessary parts

--- app/utils/enum.ts
+++ app/utils/enum.ts
@@ -1,10 +1,7 @@
 export enum API_URL {
   USER = '/api/user',
   SIGNUP = '/api/signup',
-  ALL_FORMS = '/api/allForms',
   FORM = '/api/form',
-  INPUT = '/api/input',
-  POSITION = '/api/position',
   SHEETS = '/api/sheets',
   IMPORT_SHEETS = '/api/import-sheets',
 }
--- pages/api/form/index.ts
+++ pages/api/form/index.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { POSTMethod } from './POST';
 import { GETMethod } from './GET';
 import withAuthGuard from '../utils/withAuthGuard';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const prisma = new PrismaClient();
   try {
-    if (req.method === 'POST') {
-      const response = await POSTMethod(req, res, prisma);
-      return response;
-    }
     if (req.method === 'GET') {
       const response = await GETMethod(req, res, prisma);
       return response;
