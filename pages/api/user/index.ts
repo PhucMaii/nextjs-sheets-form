f4fa8263fb10a3636aa1840ee1a40546d59dbae6
PhucMaii <maithienphuc0102@gmail.com>
2024-02-26 09:17:55 -0800

    Disable update user

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,17 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import GET from './GET';
-import PUT from './PUT';
 import withAuthGuard from '../utils/withAuthGuard';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
       const response = await GET(req, res);
-      return response;
-    }
-
-    if (req.method === 'PUT') {
-      const response = await PUT(req, res);
       return response;
     }
   } catch (error) {
