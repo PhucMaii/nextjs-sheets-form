d3a224839c307cadd8ab601ebedbb677d2abbe37
unknown <maithienphuc0102@gmail.com>
2024-05-09 10:08:19 -0700

    fix: decrease the endDate by one

--- pages/api/utils/date.ts
+++ pages/api/utils/date.ts
@@ -4,7 +4,7 @@ export const filterDateRangeOrders = (
   orders: Orders[],
   startDate: string,
   endDate: string,
-) => {
+): Orders[] => {
   const filteredDateRangeOrders = orders.filter((order: Orders) => {
     const parts = order.deliveryDate.split('/');
     const month = parseInt(parts[0], 10) - 1; // Months are 0-indexed in JavaScript, so subtract 1
@@ -12,8 +12,12 @@ export const filterDateRangeOrders = (
     const year = parseInt(parts[2], 10);
     const orderDeliveryDate = new Date(year, month, day);
 
-    const formattedStartDate = new Date(startDate);
-    const formattedEndDate = new Date(endDate);
+    const formattedStartDate = new Date(Date.parse(startDate));
+    const formattedEndDate = new Date(Date.parse(endDate));
+
+    formattedEndDate.setDate(formattedEndDate.getDate() - 1);
+    formattedStartDate.setDate(formattedStartDate.getDate() - 1);
+
 
     return (
       orderDeliveryDate >= formattedStartDate &&
