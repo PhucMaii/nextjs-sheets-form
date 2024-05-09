34baccbc0682c60eae32c1405fa526daf202ed15
unknown <maithienphuc0102@gmail.com>
2024-05-09 10:18:07 -0700

    fix: Sort order by deliveryDate

--- pages/api/utils/date.ts
+++ pages/api/utils/date.ts
@@ -1,16 +1,22 @@
 import { Orders } from '@prisma/client';
 
+const convertDeliveryDateStringToDate = (deliveryDate: string) => {
+  const parts = deliveryDate.split('/');
+  const month = parseInt(parts[0], 10) - 1; // Months are 0-indexed in JavaScript, so subtract 1
+  const day = parseInt(parts[1], 10);
+  const year = parseInt(parts[2], 10);
+  const orderDeliveryDate = new Date(year, month, day);
+
+  return orderDeliveryDate;
+}
+
 export const filterDateRangeOrders = (
   orders: Orders[],
   startDate: string,
   endDate: string,
 ): Orders[] => {
   const filteredDateRangeOrders = orders.filter((order: Orders) => {
-    const parts = order.deliveryDate.split('/');
-    const month = parseInt(parts[0], 10) - 1; // Months are 0-indexed in JavaScript, so subtract 1
-    const day = parseInt(parts[1], 10);
-    const year = parseInt(parts[2], 10);
-    const orderDeliveryDate = new Date(year, month, day);
+    const orderDeliveryDate = convertDeliveryDateStringToDate(order.deliveryDate);
 
     const formattedStartDate = new Date(Date.parse(startDate));
     const formattedEndDate = new Date(Date.parse(endDate));
@@ -25,5 +31,18 @@ export const filterDateRangeOrders = (
     );
   });
 
-  return filteredDateRangeOrders;
+  const sortedOrders = sortByDeliveryDate(filteredDateRangeOrders);
+
+  return sortedOrders;
 };
+
+const sortByDeliveryDate = (orders: Orders[]) => {
+  const sortedOrders = orders.sort((orderA, orderB) => {
+    const deliveryDateA: any = convertDeliveryDateStringToDate(orderA.deliveryDate);
+    const deliveryDateB: any = convertDeliveryDateStringToDate(orderB.deliveryDate);
+
+    return deliveryDateA - deliveryDateB;
+  })
+
+  return sortedOrders;
+}
