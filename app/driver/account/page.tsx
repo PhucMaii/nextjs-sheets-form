f061c4f4a5355547c7a8ed46993f24bf37c0a74a
unknown <maithienphuc0102@gmail.com>
2024-07-09 16:29:19 -0700

    update: apply batch processing in admin overview to handle large data volume

---
+++ app/driver/account/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import { Typography } from '@mui/material'
+
+export default function AccountPage() {
+  return (
+    <Sidebar>
+      <Typography variant="h5" textAlign="center">Account</Typography>
+
+    </Sidebar>
+  )
+}
