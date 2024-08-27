96a9759ad2fbf63802bb796fa48af21f085c3d1d
PhucMaii <maithienphuc0102@gmail.com>
2024-08-27 16:21:24 -0700

    bug: fix overiview statement

--- app/utils/time.ts
+++ app/utils/time.ts
@@ -78,5 +78,7 @@ export const generateListOfDateString = (startDate: Date, endDate: Date) => {
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
-  return dates;
+  console.log(dates, 'list of dates');
+
+  return dates.slice(0, dates.length - 1);
 };
