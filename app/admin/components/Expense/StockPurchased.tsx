3b6a3f657d66e969747454b1ccfa5f8bb7254bd3
PhucMaii <maithienphuc0102@gmail.com>
2024-11-02 22:15:08 -0700

    feat: set up purchased stock modal

--- app/admin/components/Expense/StockPurchased.tsx
+++ app/admin/components/Expense/StockPurchased.tsx
@@ -2,9 +2,10 @@ import { SWRFetchData } from '@/app/utils/db';
 import { API_URL } from '@/app/utils/enum';
 import { YYYYMMDDFormat } from '@/app/utils/time'
 import useSelectDate from '@/hooks/useSelectDate'
-import { Box, Button, Divider, Grid, MenuItem, Select, TextField, Typography } from '@mui/material'
-import { m } from 'framer-motion';
-import React, { useState } from 'react'
+import { Box, Button, Divider, Grid, IconButton, MenuItem, Select, TextField, Typography } from '@mui/material';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import React, { Fragment, useState } from 'react'
+import { errorColor } from '@/theme/color';
 
 export default function StockPurchased() {
     const [newExpense, setNewExpense] = useState<any>({
@@ -25,15 +26,67 @@ export default function StockPurchased() {
 
     const [inventoryItems] = SWRFetchData(`${API_URL.ADMIN}/inventory`);
 
+    const selectPromptedItem = (e: any) => {
+        const newId = +e.target.value;
 
+        const item = inventoryItems?.data?.find((item: any) => {
+            return item.id === newId;
+        });
+
+        setPromptedItem({
+            ...promptedItem,
+            itemId: newId,
+            unitPrice: item?.price,
+        })
+    }
+    
     // TODO: Add prompted item into purchased items
+    const addPromptedItem = () => {
+        setPurchasedItems([...purchasedItems, promptedItem]);
+        setPromptedItem({
+            itemId: -1,
+            quantity: 0,
+            unitPrice: 0,
+        })
+    }
+
+    const handleChangeItem = (e: any, targetItem: any, keyChange: string) => {
+        e.preventDefault();
+        const newItemList = purchasedItems.map((item: any) => {
+          if (item.id === targetItem.id) {
+            if (keyChange === 'quantity') {
+              const totalPrice = item.price * +e.target.value;
+              return { ...item, quantity: +e.target.value, totalPrice };
+            }
+            if (keyChange === 'price') {
+              const totalPrice = item.quantity * +e.target.value;
+              return { ...item, price: +e.target.value, totalPrice };
+            }
+            return item;
+          }
+    
+          return item;
+        });
+    
+        setPurchasedItems(newItemList);
+      };
+
+      const removeItem = (itemName: string) => {
+        const newItemList = purchasedItems.filter((item: any) => {
+          return item.name !== itemName;
+        });
+    
+        setPurchasedItems(newItemList);
+      };
+
+
     // TODO: /api/inventory/expense to add expense for stock purchased
   return (
     <Box display="flex" flexDirection="column" gap={3}>
         <Grid container spacing={3}>
             <Grid item xs={12}> 
-                <Select fullWidth value={promptedItem.itemId}>
-                    <MenuItem value={-1}>-- Choose an item --</MenuItem>
+                <Select fullWidth value={promptedItem.itemId} onChange={selectPromptedItem}>
+                    <MenuItem value={-1} disabled>-- Choose an item --</MenuItem>
                     {
                         inventoryItems && inventoryItems?.data?.map((item: any) => {
                             return (
@@ -50,6 +103,7 @@ export default function StockPurchased() {
                     label="Unit Price"
                     type="number"
                     value={promptedItem.unitPrice}
+                    onChange={(e: any) => setPromptedItem({...promptedItem, unitPrice: +e.target.value})}
                 />
             </Grid>
             <Grid item xs={12} md={6}> 
@@ -57,17 +111,60 @@ export default function StockPurchased() {
                     fullWidth
                     label="Quantity"
                     type="number"
-                    value={promptedItem.unitPrice}
+                    value={promptedItem.quantity}
+                    onChange={(e: any) => setPromptedItem({...promptedItem, quantity: +e.target.value})}
+
                 />
             </Grid>
 
             <Grid item xs={12}>
-                <Button variant="contained" fullWidth>Add</Button>
+                <Button variant="contained" fullWidth onClick={addPromptedItem}>Add</Button>
             </Grid>
         </Grid>
 
         <Divider sx={{my: 2}}>Expense Information</Divider>
 
+        {purchasedItems.length > 0 &&
+                purchasedItems.map((item: any, index) => {
+                  return (
+                    <Fragment key={index}>
+                      <Grid item xs={12} fontWeight="bold">
+                        <Box display="flex" alignItems="center" gap={1}>
+                          <Typography variant="h6" fontWeight="bold">
+                            {item.name}
+                          </Typography>
+                          <IconButton onClick={() => removeItem(item.name)}>
+                            <RemoveCircleIcon sx={{ color: errorColor }} />
+                          </IconButton>
+                        </Box>
+                      </Grid>
+                      <Grid item container columnSpacing={2}>
+                        <Grid item xs={6} textAlign="right">
+                          <TextField
+                            fullWidth
+                            label="Unit Price ($)"
+                            value={item.price}
+                            onChange={(e) => handleChangeItem(e, item, 'price')}
+                            type="number"
+                            inputProps={{ min: 0 }}
+                          />
+                        </Grid>
+                        <Grid item xs={6}>
+                          <TextField
+                            fullWidth
+                            label="Quantity"
+                            value={item.quantity}
+                            onChange={(e) =>
+                              handleChangeItem(e, item, 'quantity')
+                            }
+                            type="number"
+                            inputProps={{ min: 0 }}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Fragment>
+                  );
+                })}
         <Box display="flex" flexDirection="column" gap={2}>
             <Typography variant="h6">Date</Typography>
             {SelectDate}
