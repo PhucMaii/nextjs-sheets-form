bec08f7f86bcd6f46a4dea34ebeb9c5258c3663d
PhucMaii <maithienphuc0102@gmail.com>
2024-11-08 20:24:00 -0800

    bug: fix statement sending

--- pages/api/admin/clients/debt/index.ts
+++ pages/api/admin/clients/debt/index.ts
@@ -58,7 +58,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 export default withAdminAuthGuard(handler);
 
-export const groupOrderByMMYYYY = (orders: Orders[], endMonth: string) => {
+export const groupOrderByMMYYYY = (orders: Orders[], endDate: string) => {
+  const endMonth = endDate.split('/')[0];
+  console.log(endMonth, 'endMonth');
   const debtOrdersByMonth = orders.reduce((acc: any, order: Orders) => {
     const splitDeliveryDate = order.deliveryDate.split('/');
     if (Number(splitDeliveryDate[0]) > Number(endMonth)) {
