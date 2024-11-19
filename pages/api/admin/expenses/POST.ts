c2adcc9412f09153df0141c15be0be152d355546
PhucMaii <maithienphuc0102@gmail.com>
2024-11-18 21:13:46 -0800

    bug: fix cod board data

--- pages/api/admin/expenses/POST.ts
+++ pages/api/admin/expenses/POST.ts
@@ -38,8 +38,6 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
-    console.log({ paymentMethodId, existingMethod }, 'paymentMethodId');
-
     if (!existingMethod) {
       return res.status(404).json({
         error: 'Payment Method Not Found',
--- pages/api/admin/inventory/expenses/POST.ts
+++ pages/api/admin/inventory/expenses/POST.ts
@@ -55,25 +55,27 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
       return acc;
     }, []);
 
-    // Check if vendor has expense on that date
-    const existingVendorExpense = await prisma.expense.findMany({
-      where: {
-        invoice: invoice,
-        date: date,
-        vendors: {
-          some: {
-            vendorId: {
-              in: vendors,
+    if (invoice && invoice?.trim() !== '') {
+      // Check if vendor has expense on that date
+      const existingVendorExpense = await prisma.expense.findMany({
+        where: {
+          invoice: invoice,
+          date: date,
+          vendors: {
+            some: {
+              vendorId: {
+                in: vendors,
+              },
             },
           },
         },
-      },
-    });
-
-    if (existingVendorExpense.length > 0) {
-      return res
-        .status(409)
-        .json({ error: `Expense Already Exists For ${invoice}` });
+      });
+  
+      if (existingVendorExpense.length > 0) {
+        return res
+          .status(409)
+          .json({ error: `Expense Already Exists For ${invoice}` });
+      }
     }
 
     const newExpense = await prisma.expense.create({
