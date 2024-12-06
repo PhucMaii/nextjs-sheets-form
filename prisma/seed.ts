1008363ab9aab1dbb2092e2abec1f0591fe5f667
PhucMaii <maithienphuc0102@gmail.com>
2024-12-06 12:44:30 -0800

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -42,132 +42,172 @@ export const generateListOfDateString = (startDate: Date, endDate: Date) => {
 };
 
 const prisma = new PrismaClient();
-const checkIsKorean = (text: string) => {
-  // const koreanRange = /^[\uAC00-\uD7AF]+$/;
-  const koreanRange = /[\uAC00-\uD7AF]/;
-  return koreanRange.test(text);
-};
+// const checkIsKorean = (text: string) => {
+//   // const koreanRange = /^[\uAC00-\uD7AF]+$/;
+//   const koreanRange = /[\uAC00-\uD7AF]/;
+//   return koreanRange.test(text);
+// };
 
 
 async function main() {
-  const allScheduledOrderedItemsHasNoInventory = await prisma.orderedItems.findMany({
-    where: {
-      scheduledOrderId: {
-        not: null
-      },
-      inventoryItemId: null
-    }
-  });
+  // const allScheduledOrderedItemsHasNoInventory = await prisma.orderedItems.findMany({
+  //   where: {
+  //     scheduledOrderId: {
+  //       not: null
+  //     },
+  //     inventoryItemId: null
+  //   }
+  // });
 
-  const inventoryItems = await prisma.inventoryItem.findMany({
+  // const inventoryItems = await prisma.inventoryItem.findMany({
+  //   include: {
+  //     vendorItem: {
+  //       include: {
+  //         unit: true
+  //       }
+  //     }
+  //   }
+  // });
+
+
+  // const newItems = allScheduledOrderedItemsHasNoInventory.map((item: any) => {
+  //   let itemKey = item.name;
+  //   let inventoryUnitIndex = 0;
+
+  //   if (checkIsKorean(itemKey.split(' - ')[0])) {
+  //     itemKey = itemKey.split(' - ')[1];
+  //   } else {
+  //     itemKey = itemKey.includes('KONGNAMUL')
+  //       ? itemKey.split(' - ')[1]
+  //       : itemKey;
+  //   }
+
+  //   if (itemKey === 'BEAN 24X1 LB') {
+  //     itemKey = 'BEAN 1 LB';
+  //     inventoryUnitIndex = 1;
+  //   }
+
+  //   if (itemKey === 'OYSTER MUSHROOM #1') {
+  //     itemKey = 'NO. 1 OYSTER MUSHROOM 5 LB'
+  //   }
+
+  //   if (itemKey === 'OYSTER MUSHROOM #2') {
+  //     itemKey = 'NO. 2 OYSTER MUSHROOM 5 LB'
+  //   }
+
+  //   if (itemKey === 'TARO 40 LB') {
+  //     itemKey = 'TARO'
+  //   }
+
+  //   if (itemKey === 'BASIL BOX') {
+  //     itemKey = 'BASIL';
+  //     inventoryUnitIndex = 1;
+  //   }
+
+  //   if (itemKey === 'SOYA 24X1 LB') {
+  //     itemKey = 'SOYA 1 LB';
+  //     inventoryUnitIndex = 1;
+  //   }
+
+  //   if (itemKey === 'KOREAN SOYA 1X24') {
+  //     itemKey = 'K. SOYA 1 LB';
+  //     inventoryUnitIndex = 1;
+  //   }
+
+  //   if (itemKey === 'BEAN 5X1 LB') {
+  //     itemKey = 'BEAN 1 LB';
+  //     inventoryUnitIndex = 2;
+  //   }
+
+  //   if (itemKey === 'FRESH RICE NOODLE 1 LB') {
+  //     itemKey = 'FRESH RICE NOODLES 1 LB';
+  //     inventoryUnitIndex = 0;
+  //   }
+
+  //   if (itemKey === 'LAGRE EGG') {
+  //     itemKey = 'LARGE EGG';
+  //     inventoryUnitIndex = 0;
+  //   }
+
+  //   if (itemKey === 'WHITE MUSHROOM') {
+  //     itemKey = 'No. 1 MUSHROOM WHITE 10 LB';
+  //     inventoryUnitIndex = 0;
+  //   }
+
+  //   const sameNameInventory = inventoryItems.find((i: any) => {
+  //     return i.name == itemKey;
+  //   });
+
+  //   if (!sameNameInventory) {
+  //     console.error('Inventory item not found: ' + itemKey);
+  //     return item;
+  //   }
+
+  //   console.log('Inventory item found: ' + item.name);
+
+  //   return {
+  //     name: item.name,
+  //     price: item.price,
+  //     quantity: item.quantity,
+  //     scheduledOrderId: item.scheduledOrderId,
+  //     inventoryItemId: sameNameInventory.id,
+  //     inventoryUnitId: sameNameInventory.vendorItem[0].unit[inventoryUnitIndex].id,
+  //   }
+  // });
+
+  // await prisma.orderedItems.deleteMany({
+  //   where: {
+  //     scheduledOrderId: {
+  //       not: null
+  //     },
+  //     inventoryItemId: null
+  //   }
+  // });
+
+
+  // await prisma.orderedItems.createMany({
+  //   data: newItems
+  // })
+
+  const users = await prisma.user.findMany({
     include: {
-      vendorItem: {
+      scheduleOrders: true,
+      routes: {
         include: {
-          unit: true
+          route: true
         }
-      }
+      },
     }
   });
 
-
-  const newItems = allScheduledOrderedItemsHasNoInventory.map((item: any) => {
-    let itemKey = item.name;
-    let inventoryUnitIndex = 0;
-
-    if (checkIsKorean(itemKey.split(' - ')[0])) {
-      itemKey = itemKey.split(' - ')[1];
-    } else {
-      itemKey = itemKey.includes('KONGNAMUL')
-        ? itemKey.split(' - ')[1]
-        : itemKey;
-    }
-
-    if (itemKey === 'BEAN 24X1 LB') {
-      itemKey = 'BEAN 1 LB';
-      inventoryUnitIndex = 1;
-    }
-
-    if (itemKey === 'OYSTER MUSHROOM #1') {
-      itemKey = 'NO. 1 OYSTER MUSHROOM 5 LB'
-    }
-
-    if (itemKey === 'OYSTER MUSHROOM #2') {
-      itemKey = 'NO. 2 OYSTER MUSHROOM 5 LB'
-    }
-
-    if (itemKey === 'TARO 40 LB') {
-      itemKey = 'TARO'
-    }
-
-    if (itemKey === 'BASIL BOX') {
-      itemKey = 'BASIL';
-      inventoryUnitIndex = 1;
-    }
-
-    if (itemKey === 'SOYA 24X1 LB') {
-      itemKey = 'SOYA 1 LB';
-      inventoryUnitIndex = 1;
-    }
-
-    if (itemKey === 'KOREAN SOYA 1X24') {
-      itemKey = 'K. SOYA 1 LB';
-      inventoryUnitIndex = 1;
-    }
-
-    if (itemKey === 'BEAN 5X1 LB') {
-      itemKey = 'BEAN 1 LB';
-      inventoryUnitIndex = 2;
-    }
-
-    if (itemKey === 'FRESH RICE NOODLE 1 LB') {
-      itemKey = 'FRESH RICE NOODLES 1 LB';
-      inventoryUnitIndex = 0;
-    }
-
-    if (itemKey === 'LAGRE EGG') {
-      itemKey = 'LARGE EGG';
-      inventoryUnitIndex = 0;
-    }
-
-    if (itemKey === 'WHITE MUSHROOM') {
-      itemKey = 'No. 1 MUSHROOM WHITE 10 LB';
-      inventoryUnitIndex = 0;
-    }
-
-    const sameNameInventory = inventoryItems.find((i: any) => {
-      return i.name == itemKey;
+  for (const user of users) {
+    const userRoutes = user.routes.map((route) => {
+      return route.route.day;
     });
 
-    if (!sameNameInventory) {
-      console.error('Inventory item not found: ' + itemKey);
-      return item;
+    const conflictScheduleOrders = user.scheduleOrders.filter((scheduleOrder) => {
+      return !userRoutes.includes(scheduleOrder.day);
+    });
+
+    if (conflictScheduleOrders.length > 0) {
+      console.log({
+        name: user.clientName,
+        id: user.id,
+        scheduleOrders: conflictScheduleOrders
+      });
+      // const scheduleOrderIds = conflictScheduleOrders.map((scheduleOrder) => {
+      //   return scheduleOrder.id;
+      // });
+
+      // await prisma.scheduleOrders.deleteMany({
+      //   where: {
+      //     id: {
+      //       in: scheduleOrderIds
+      //     }
+      //   }
+      // });
     }
-
-    console.log('Inventory item found: ' + item.name);
-
-    return {
-      name: item.name,
-      price: item.price,
-      quantity: item.quantity,
-      scheduledOrderId: item.scheduledOrderId,
-      inventoryItemId: sameNameInventory.id,
-      inventoryUnitId: sameNameInventory.vendorItem[0].unit[inventoryUnitIndex].id,
-    }
-  });
-
-  await prisma.orderedItems.deleteMany({
-    where: {
-      scheduledOrderId: {
-        not: null
-      },
-      inventoryItemId: null
-    }
-  });
-
-
-  await prisma.orderedItems.createMany({
-    data: newItems
-  })
+  }
 }
 
 main()
