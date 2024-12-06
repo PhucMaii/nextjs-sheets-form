a23c81f0d49a91096200bf08abd8990e8f8922d3
PhucMaii <maithienphuc0102@gmail.com>
2024-12-06 10:14:30 -0800

    bug: fix rearrangement

--- pages/api/admin/scheduledOrders/reArrangement.ts
+++ pages/api/admin/scheduledOrders/reArrangement.ts
@@ -46,6 +46,8 @@ export default async function reArrangement(
           price: item.price,
           quantity: item.quantity,
           scheduledOrderId: scheduledOrder.id,
+          inventoryItemId: item.inventoryItemId,
+          inventoryUnitId: item.inventoryUnitId,
         };
       });
 
