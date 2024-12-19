506388cffead7b828a01878ec1d6a2d86d605f4b
PhucMaii <maithienphuc0102@gmail.com>
2024-12-18 21:07:40 -0800

    update: add item type in db

--- app/components/ProductListingPage/ProductHeader.tsx
+++ app/components/ProductListingPage/ProductHeader.tsx
@@ -13,49 +13,49 @@ import { landingPageSecondaryColor } from '@/constant/landingPage';
 
 export default function ProductHeader() {
   return (
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap={6}
-        px={6}
-        py={12}
-        sx={{ backgroundColor: green[700], borderRadius: 5 }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={6}
+      px={6}
+      py={12}
+      sx={{ backgroundColor: green[700], borderRadius: 5 }}
+    >
+      <Typography
+        variant="h3"
+        textAlign="center"
+        fontWeight="bold"
+        sx={{ color: 'white', lineHeight: 1.5 }}
       >
-        <Typography
-          variant="h3"
-          textAlign="center"
-          fontWeight="bold"
-          sx={{ color: 'white', lineHeight: 1.5 }}
-        >
-          Fresh, Handpicked, and Nutrient-Rich: Discover Our Premium Vegetable
-          Selection Today!
-        </Typography>
+        Fresh, Handpicked, and Nutrient-Rich: Discover Our Premium Vegetable
+        Selection Today!
+      </Typography>
 
-        <Box display="flex" alignItems="center" gap={2} px={6}>
-          <FormControl fullWidth>
-            <OutlinedInput
-              placeholder="Search items you like..."
-              size="small"
-              sx={{ borderRadius: 2, backgroundColor: 'white' }}
-              fullWidth
-              startAdornment={
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <Button
-            variant="contained"
-            sx={{
-              width: 'fit-content',
-              backgroundColor: landingPageSecondaryColor,
-              ':hover': { backgroundColor: orange[800] },
-            }}
-          >
-            Search
-          </Button>
-          {/* <TextField
+      <Box display="flex" alignItems="center" gap={2} px={6}>
+        <FormControl fullWidth>
+          <OutlinedInput
+            placeholder="Search items you like..."
+            size="small"
+            sx={{ borderRadius: 2, backgroundColor: 'white' }}
+            fullWidth
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          sx={{
+            width: 'fit-content',
+            backgroundColor: landingPageSecondaryColor,
+            ':hover': { backgroundColor: orange[800] },
+          }}
+        >
+          Search
+        </Button>
+        {/* <TextField
                         label="Search Products"
                         placeholder="Search items you like..."
                         size="small"
@@ -73,7 +73,7 @@ export default function ProductHeader() {
                             },
                           }}
                     /> */}
-        </Box>
       </Box>
+    </Box>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 import React from 'react';
-import {
-  Box,
-} from '@mui/material';
+import { Box } from '@mui/material';
 import Navbar from '../components/LandingPage/Navbar';
 import ProductHeader from '../components/ProductListingPage/ProductHeader';
 
@@ -10,9 +8,9 @@ export default function ProductPage() {
   return (
     <Box>
       <Navbar />
-        <Box display="flex" flexDirection="column" gap={2} py={3} px={6}>
-            <ProductHeader />
-        </Box>
+      <Box display="flex" flexDirection="column" gap={2} py={3} px={6}>
+        <ProductHeader />
+      </Box>
     </Box>
   );
 }
