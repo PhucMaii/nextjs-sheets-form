3197fd627c37c51f3f18dc8d89a2c66fc74f2bb8
PhucMaii <maithienphuc0102@gmail.com>
2024-12-26 12:04:34 -0800

    not important

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -246,18 +246,7 @@ async function main() {
   //     });
   //   }
   // }
-
-  const orders = await prisma.orders.findMany({
-    where: {
-      orderTime: {
-        not: '23:39:32 2024-12-22',
-      },
-      deliveryDate: '12/31/2024',
-    },
-  });
-
-  console.log(orders, 'orders');
-
+ 
   // const scheduledOrders = await prisma.scheduleOrders.findMany({
   //   include: {
   //     items: true,
