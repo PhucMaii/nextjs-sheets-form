e13503923e821c2156f3f278340dc1b4c9e415c7
unknown <maithienphuc0102@gmail.com>
2025-02-03 07:03:48 -0800

    remove memo

--- app/admin/components/Printing/AllPrint.tsx
+++ app/admin/components/Printing/AllPrint.tsx
@@ -1,8 +1,7 @@
 import React, { forwardRef, memo } from 'react';
-import { ComponentToPrint } from './ComponentToPrint';
 import './print.css';
+import { ComponentToPrint } from './ComponentToPrint';
 
-const MemoizedComponentToPrint = memo(ComponentToPrint);
 
 const AllPrint = forwardRef(({ orders }: { orders: any }, ref: any) => {
   return (
@@ -10,7 +9,7 @@ const AllPrint = forwardRef(({ orders }: { orders: any }, ref: any) => {
       {orders.map((order: any, index: number) => {
         return (
           <React.Fragment key={index}>
-            <MemoizedComponentToPrint order={order} ref={order.ref} />
+            <ComponentToPrint order={order} ref={order.ref} />
             {index < orders.length - 1 && <div className="page-break"></div>}
           </React.Fragment>
         );
