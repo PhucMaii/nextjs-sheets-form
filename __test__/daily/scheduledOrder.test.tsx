988796d73488421c95cc2626a33de62c86f5196c
PhucMaii <maithienphuc0102@gmail.com>
2025-02-10 20:34:11 -0800

    bug: fix the path with role

--- __test__/daily/scheduledOrder.test.tsx
+++ __test__/daily/scheduledOrder.test.tsx
@@ -41,6 +41,13 @@ describe('Pre order check', () => {
         continue;
       }
       if (scheduleOrderItem.isShowDiscount !== categoryItem.isShowDiscount) {
+        if (scheduleOrderItem.isShowDiscount === false && categoryItem.isShowDiscount === null) {
+          continue;
+        } 
+
+        if (categoryItem.isShowDiscount === false && scheduleOrderItem.isShowDiscount === null) {
+          continue;
+        }
         console.error(
           {
             clientName: scheduleOrderItem?.ScheduleOrders?.user.clientName,
