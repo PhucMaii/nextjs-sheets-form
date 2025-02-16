5e5976d34186d89322891d69c1b48026b2e7fb7c
PhucMaii <maithienphuc0102@gmail.com>
2025-02-15 21:30:00 -0800

    add test for transactiong

--- __test__/api/admin/transactions/createTransaction.test.tsx
+++ __test__/api/admin/transactions/createTransaction.test.tsx
@@ -1,3 +1,175 @@
+import { mainPaymentMethodId } from '@/app/lib/constant';
+import { TRANSACTION_STATUS } from '@/app/utils/enum';
+import {
+  checkAndUpdateUnits,
+  createFifo,
+  createOrderedItems,
+  updateVendorItemQuantity,
+} from '@/pages/api/admin/inventory/expenses/POST';
+import { getTodayDate } from '@/pages/api/utils/date';
+import { PrismaClient } from '@prisma/client';
+
 describe('Create Transaction', () => {
-    
-})
+  test('Create normal transaction with link inventory', async () => {
+    const prisma = new PrismaClient();
+
+    const { date, time } = getTodayDate();
+    // const orderDate = '01/01/3000';
+
+    const amount = 1500;
+    const paymentMethodId = mainPaymentMethodId;
+
+    const createdAt = `${date} ${time}`;
+    const description = 'Test Transaction';
+
+    const subTotal = 1500;
+    const GST = 0;
+    const PST = 0;
+    const items: any = [
+      {
+        id: 3,
+        vendorId: 4,
+        quantity: 100,
+        unitPrice: 0,
+        unit: {
+          id: 3,
+          vendorItemId: 3,
+          unit: 'bags',
+          unitPrice: 3.75,
+          ratio: 1,
+          createdAt: '09:18:46 2024-12-03',
+          createdBy: 'Admin - Admin Test',
+        },
+        units: [
+          {
+            createdAt: '09:18:46 2024-12-03',
+            createdBy: 'Admin - Admin Test',
+            id: 3,
+            ratio: 1,
+            unit: 'bags',
+            unitPrice: 3.75,
+            vendorItemId: 3,
+          },
+        ],
+        name: 'BEAN 5 LB',
+        inventoryItemId: 19,
+        inventoryItem: {
+          id: 19,
+          name: 'BEAN 5 LB',
+          hasPST: false,
+          hasGST: false,
+          createdAt: '08:10:18 2024-11-07',
+          createdBy: 'Admin - Bao Bao',
+        },
+      },
+      {
+        id: 2,
+        quantity: 150,
+        name: 'BEAN 10 LB',
+        vendorId: 4,
+        unit: {
+          id: 2,
+          vendorItemId: 2,
+          unit: 'bags',
+          unitPrice: 7.5,
+          ratio: 1,
+          createdAt: '09:18:46 2024-12-03',
+          createdBy: 'Admin - Admin Test',
+        },
+        units: [
+          {
+            createdAt: '09:18:46 2024-12-03',
+            createdBy: 'Admin - Admin Test',
+            id: 2,
+            ratio: 1,
+            unit: 'bags',
+            unitPrice: 7.5,
+            vendorItemId: 2,
+          },
+        ],
+        inventoryItemId: 18,
+        inventoryItem: {
+          id: 18,
+          name: 'BEAN 10 LB',
+          hasPST: false,
+          hasGST: false,
+          createdAt: '08:10:18 2024-11-07',
+          createdBy: 'Admin - Bao Bao',
+        },
+        totalPrice: null,
+      },
+    ];
+
+    const createdBy = 'Admin - Admin Test';
+    // Create expense
+    const newExpense = await prisma.expense.create({
+      data: {
+        invoice: 'Test Invoice',
+        date: date,
+        amount: amount,
+        subTotal: subTotal,
+        PST: PST,
+        GST: GST,
+        description: description,
+        paymentMethodId: paymentMethodId,
+        spentBy: createdBy,
+        createdAt: createdAt,
+        codBoardId: null,
+        status: TRANSACTION_STATUS.PAID,
+        createdBy,
+      },
+    });
+
+    // Connect vendors and expenses
+    await prisma.vendorExpense.createMany({
+      data: {
+        expenseId: newExpense.id,
+        vendorId: 4,
+      },
+    });
+
+    // const inventoryItems = await prisma.inventoryItem.findMany({});
+
+    // Create batch for newly imported items
+    await createFifo(items, createdAt, createdBy);
+
+    const vendorItems = await prisma.vendorItem.findMany({
+      where: {
+        id: {
+          in: items.map((item: any) => item.id),
+        },
+      },
+      include: {
+        unit: true,
+      },
+    });
+
+    for (const item of items) {
+      // STEP 2: Update quantity in vendor items
+      const existedItem: any = vendorItems.find(
+        (vendorItem) => vendorItem.id === item.id,
+      );
+
+      await updateVendorItemQuantity(existedItem, item);
+
+      // STEP 3: Check unit price in Inventory Unit (Update if needed)
+      await checkAndUpdateUnits(
+        existedItem.unit,
+        item.units,
+        item.id,
+        createdAt,
+        createdBy,
+      );
+    }
+    // STEP 4: Create OrderedItems
+    // Use item already exist to easy to retrieve unitPrice
+    const response = await createOrderedItems(
+      items,
+      newExpense,
+      createdAt,
+      createdBy,
+    );
+
+    expect(response.ok).toBeTruthy();
+  });
+});
