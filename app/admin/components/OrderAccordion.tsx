750fccd33bc6cee988dea76f3381fe8762336f06
PhucMaii <maithienphuc0102@gmail.com>
2025-02-21 12:48:40 -0800

    update: add descriptive log into pre order

--- app/admin/components/OrderAccordion.tsx
+++ app/admin/components/OrderAccordion.tsx
@@ -280,7 +280,7 @@ const OrderAccordion = ({
           disabled={order?.type === TYPE.LOCKED}
           onClick={() => setIsOpenEditPrice(true)}
         >
-          Edit price
+          Edit order
         </MenuItem>
         <MenuItem
           onClick={(e) => {
