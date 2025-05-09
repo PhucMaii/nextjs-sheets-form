f5c0924c01490dd77be604663f73e7fc8240e645
PhucMaii <maithienphuc0102@gmail.com>
2025-05-08 20:46:12 -0700

    fix bug add the quantity if the itemKey exists in manifest

--- pages/api/admin/manifest/index.ts
+++ pages/api/admin/manifest/index.ts
@@ -298,6 +298,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             const newUserManifest = {
               user: { ...user, displayName },
               order,
+              // TODO: Fix bug ADD quantity if the itemKey is already exist
               [itemKey]: actualQuantity,
             };
 
@@ -309,10 +310,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           const updatedUserManifest = {
             ...currentUserManifest,
             order,
-            [itemKey]: actualQuantity,
+            [itemKey]: (currentUserManifest[itemKey] || 0) + actualQuantity, // Handle if the itemKey is already exist, then add the quantity
           };
 
-          // console.log(updatedUserManifest.user.clientName, 'updatedUserManifest');
           acc[acc.length - 1] = updatedUserManifest;
           return acc;
         },
