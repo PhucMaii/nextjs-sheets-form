95fb03bb5374816e6ef8f1f4fbe6a982d8e4b79f
PhucMaii <maithienphuc0102@gmail.com>
2025-05-23 18:14:08 -0700

    refactor the shift session page

---
+++ app/admin/[companyId]/shifts/ScheduledShifts.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function ScheduledShifts() {
+  return (
+    <>
+    <Typography variant='h6'>Schedule</Typography>
+    </>
+  )
+}
