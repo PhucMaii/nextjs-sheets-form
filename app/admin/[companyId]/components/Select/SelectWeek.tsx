ac71469d4f0181adab6fe5ad23fdce5b56b7bfe8
PhucMaii <maithienphuc0102@gmail.com>
2025-05-28 10:31:58 -0700

    feat: add navigation for seleting week

--- app/admin/[companyId]/components/Select/SelectWeek.tsx
+++ app/admin/[companyId]/components/Select/SelectWeek.tsx
@@ -1,7 +1,9 @@
-import { Button, Dialog, DialogContent } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, IconButton } from '@mui/material';
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import Calendar from 'react-calendar';
 import { startOfWeek, endOfWeek, format } from 'date-fns';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import 'react-calendar/dist/Calendar.css';
 
@@ -22,6 +24,26 @@ export default function SelectWeek({
   const handleDateOpen = () => setShowDatePicker(true);
   const handleDateClose = () => setShowDatePicker(false);
 
+  const handlePreviousWeek = () => {
+    const lastStartDate = new Date(selectedWeek[0]);
+    lastStartDate.setDate(lastStartDate.getDate() - 7);
+
+    const lastEndDate = new Date(selectedWeek[1]);
+    lastEndDate.setDate(lastEndDate.getDate() - 7);
+
+    setSelectedWeek([lastStartDate, lastEndDate]);
+  };
+
+  const handleNextWeek = () => {
+    const nextStartDate = new Date(selectedWeek[0]);
+    nextStartDate.setDate(nextStartDate.getDate() + 7);
+
+    const nextEndDate = new Date(selectedWeek[1]);
+    nextEndDate.setDate(nextEndDate.getDate() + 7);
+
+    setSelectedWeek([nextStartDate, nextEndDate]);
+  };
+
   const handleWeekSelect = (date: Date) => {
     setSelectedWeek([
       startOfWeek(date, { weekStartsOn: 1 }),
@@ -33,19 +55,26 @@ export default function SelectWeek({
   const renderSelectedWeek = () => {
     if (!selectedWeek || selectedWeek.length === 0) {
       return (
-        <Button variant={variant || 'outlined'} onClick={handleDateOpen}>
-          Select Week
-        </Button>
+        <Box display="flex" justifyContent="center" alignItems="center" gap={1}>
+          <Button variant={variant || 'outlined'} onClick={handleDateOpen}>
+            Select Week
+          </Button>
+        </Box>
       );
     }
 
-    // const start = startOfWeek(selectedWeek, { weekStartsOn: 1 });
-    // const end = endOfWeek(selectedWeek, { weekStartsOn: 1 });
-
     return (
-      <Button variant={variant || 'outlined'} onClick={handleDateOpen}>
-        {`${format(selectedWeek[0], 'MMM d')} – ${format(selectedWeek[1], 'MMM d, yyyy')}`}
-      </Button>
+      <Box display="flex" justifyContent="center" alignItems="center" gap={1}>
+        <IconButton onClick={handlePreviousWeek}>
+          <ArrowBackIosNewIcon sx={{ fontSize: 16 }} />
+        </IconButton>
+        <Button variant={variant || 'outlined'} onClick={handleDateOpen}>
+          {`${format(selectedWeek[0], 'MMM d')} – ${format(selectedWeek[1], 'MMM d, yyyy')}`}
+        </Button>
+        <IconButton onClick={handleNextWeek}>
+          <ArrowForwardIosIcon sx={{ fontSize: 16 }} />
+        </IconButton>
+      </Box>
     );
   };
 
