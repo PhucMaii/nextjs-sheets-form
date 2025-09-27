906a8b0b4bdf1ed9b637c6da1eab542beebab75f
PhucMaii <maithienphuc0102@gmail.com>
2025-09-27 16:41:56 -0700

    anounce about basil

--- app/components/HolidayText.tsx
+++ app/components/HolidayText.tsx
@@ -1,12 +1,12 @@
 import { Box, Typography } from '@mui/material';
-import { red, yellow } from '@mui/material/colors';
+import { red } from '@mui/material/colors';
 import React from 'react';
 
-const highlightStyle = {
-  backgroundColor: yellow[800],
-  padding: 1,
-  borderRadius: 5,
-};
+// const highlightStyle = {
+//   backgroundColor: yellow[800],
+//   padding: 1,
+//   borderRadius: 5,
+// };
 
 export default function HolidayText() {
   return (
@@ -29,10 +29,12 @@ export default function HolidayText() {
           sx={{ color: 'white', lineHeight: 1.5 }}
           fontWeight="bold"
         >
-          <strong style={highlightStyle}>
+          {/* <strong style={highlightStyle}>
             All Eggs
           </strong>{' '} price will be <br />
-          increased by next week
+          increased by next week */}
+          Due to complications and shortage of Hawaii&apos;s Basil. We&apos;re currently
+          only able to supply Basil from Mexico and should be back to normal by Oct 15.
         </Typography>
         <Typography sx={{ color: 'white', textAlign: 'center' }}>
           We truly appreciate <br /> your support and understanding! 💚
